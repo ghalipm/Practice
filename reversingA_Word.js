4e091f558de35aac3a56ba2e36bd11edbd0350a2
ghalipm(Ghalip Muhammed -  Alip Mohammed - Maimaiti Alifu ) <ghalipm@gmail.com>
2022-10-13 23:59:59 -0400

    firstnonduplicate

--- reversingA_Word.js
+++ reversingA_Word.js
@@ -5,17 +5,20 @@ function reverseTheWord(word){
         newWord+=word.charAt(i);
     }
     return newWord;
-    //console.log(word+ ' reversed: '+newWord)
 }
 
 function palindrome(word) {
-    return word.toUpperCase()===reverseTheWord(word).toUpperCase();
+     return word.toUpperCase()===reverseTheWord(word).toUpperCase();
 }
 
+let mypalindrome=(word) => word.toUpperCase()===reverseTheWord(word).toUpperCase();
+
+
 //Call the function
-word='Anna'; // baba
+word='Anna'; // baba, racecar;
 console.log(word+' reversed: '+reverseTheWord(word))
 console.log('It is palindrome: '+palindrome(word));
+console.log('It is palindrome: '+mypalindrome(word));
 
 
 
